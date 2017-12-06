15a4c1d633441a1f8d76d969844006ecf50d167e
Juan Roca <juanmaa13@gmail.com>
2017-12-05 21:19:42 -0500

    covered components to the fullest with states

--- ReduxDankYoutube/src/components/search_bar.js
+++ ReduxDankYoutube/src/components/search_bar.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={ term: 'Starting value' }
+  }
+
   render() {
     return (
       <div>
-        <input/>
+        <input 
+          value= {this.state.term}
+          onChange={ event => this.setState( { term: event.target.value } ) } 
+        />
+        <p>Value of input: {this.state.term}</p>
       </div>
     );
   }
