2114ffd7721bedc8c4492ff8936b952c34f0011a
Juan Roca <juanmaa13@gmail.com>
2017-12-14 14:18:33 -0500

    added SimpleBook miniProject

---
+++ ReduxSimpleBooks/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    { title: 'Javascript: The Good parts', pages:'32'},
+    { title: 'Oh Relly?', pages:'8'},
+    { title: 'Harry Potter', pages:'208'},
+    { title: 'Hunger Games', pages:'150'},
+  ]
+}
